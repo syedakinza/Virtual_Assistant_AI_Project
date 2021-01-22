8ebfe9f05b12225d642573ceab38c3c54588b4fa
syedakinza <kinza.fatima2511@yahoo.com>
2021-01-22 18:48:40 +0500

    I have added a function which will take user's command in voice form and convert it in text form for executing them

--- Virtual_Assistant.py
+++ Virtual_Assistant.py
@@ -11,3 +11,32 @@ from datetime import date
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voices',voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+    speak("I Am Your Assistant Sir. Please Tell Me How May I Help You")
+    
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio,language='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        speak("Sir Say That Again Please...")
+        return "None"
+    return  query
